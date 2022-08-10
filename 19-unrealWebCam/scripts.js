80af6ad7f88b74f231fba76edc74df3c1dfcf59d
orbeenshr <orbeenshr@gmail.com>
2022-08-11 00:01:03 +0545

    added camera local media steam

--- 19-unrealWebCam/scripts.js
+++ 19-unrealWebCam/scripts.js
@@ -8,7 +8,10 @@ function getVideo(){
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
     .then(localMediaStream => {
         console.log(localMediaStream)
+        video.srcObject = localMediaStream
+        video.play()
     })
+    .catch(err => console.log(`OH NO!!!`, err))
 }
 
 
