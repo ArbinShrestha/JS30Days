a3527ad1a47bd4ec926fc8fda255933db25c7b1d
ArbinShrestha <orbeenshr@gmail.com>
2022-08-16 00:11:03 +0545

    function of follow along link highlighter

--- 22-followAlongLink/script.js
+++ 22-followAlongLink/script.js
@@ -4,7 +4,19 @@ highlight.classList.add('highlight')
 document.body.append(highlight)
 
 function highlightLink(){
-    console.log(this)
+    const linkCoordinates = this.getBoundingClientRect()
+    console.log(linkCoordinates)
+    const coords ={
+        width: linkCoordinates.width,
+        height: linkCoordinates.height,
+        top: linkCoordinates.top + window.scrollY,
+        left: linkCoordinates.left + window.scrollX
+    }
+    highlight.style.width = `${linkCoordinates.width}px`
+    highlight.style.height = `${linkCoordinates.height}px`
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`
+
+
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink))
