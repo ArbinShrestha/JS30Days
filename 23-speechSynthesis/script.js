13e1c84d7ef232384d7aab92d8d0bdb869c11b84
ArbinShrestha <orbeenshr@gmail.com>
2022-08-18 23:58:03 +0545

    the voice inator

--- 23-speechSynthesis/script.js
+++ 23-speechSynthesis/script.js
@@ -10,6 +10,7 @@ msg.text = document.querySelector('[name="text"]').value;
 function populateVoices() {
     voices = this.getVoices();
     voicesDropdown.innerHTML = voices
+        // .filter(voice => voice.lang.includes('en'))      //to include only en languages
         .map(voice => 
             `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
     )
@@ -18,7 +19,25 @@ function populateVoices() {
 
 function setVoice(){
     msg.voice = voices.find(voice => voice.name === this.value);
+    toggle();
+}
+
+function toggle(startOver = true){
+    speechSynthesis.cancel()
+    if(startOver){
+        speechSynthesis.speak(msg);
+    }
+    
+}
+
+function setOption(){
+    console.log(this.name, this.value)
+    msg[this.name] = this.value
+    toggle()
 }
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
 voicesDropdown.addEventListener('change', setVoice)
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', ()=>toggle(false));
